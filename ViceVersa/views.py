0a930a24f1719ed69ef32baf06949b884b7769a3
unknown <zaprudny.niks@gmail.com>
2021-03-27 02:02:34 +0500

    FirstProject

---
+++ ViceVersa/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def about(request):
+    return HttpResponse('About page')
+
+
+def home(request):
+    return render(request, 'home_page.html')
