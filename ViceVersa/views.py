273ca94419b10b1663f0189b66c7b18b431a389c
NiksForsaken <zaprudny.niks@gmail.com>
2021-03-29 18:57:10 +0500

    ViceVersaUpdate

--- ViceVersa/views.py
+++ ViceVersa/views.py
@@ -8,3 +8,9 @@ def about(request):
 
 def home(request):
     return render(request, 'home_page.html')
+
+
+def reverse(request):
+    input_user = request.GET['input']
+    reversed_input = input_user[::-1]
+    return render(request, 'reverse.html', {'input': input_user, 'reversed_input': reversed_input})
